ed887d17185368ace0485b0a3bf9e2e194c75a81
Rene <rene@sloopapp.io>
2021-11-22 12:28:33 -0600

    Initial Commit

---
+++ XSloop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XSloop
+//
+//  Created by Rene Chamorro on 11/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XSloop/XSloopApp.swift
@@ -0,0 +1,17 @@
+//
+//  XSloopApp.swift
+//  XSloop
+//
+//  Created by Rene Chamorro on 11/22/21.
+//
+
+import SwiftUI
+
+@main
+struct XSloopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
